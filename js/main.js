4d45df43be4c93f07983ac03f1811e3c05b983b4
ssilvuk <ssilvukweb@gmail.com>
2022-10-02 23:45:13 +0100

    Initial upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const nav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".nav-toggle");
+
+// when someone clicks the hamburger button
+navToggle.addEventListener("click", () => {
+    const visiblity = nav.getAttribute("data-visible");
+
+    // if the nav is closed, open it
+    if (visiblity === "false") {
+        nav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
+    }
+    // if the nav is open, close it
+    else {
+        nav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
+    }
+});
